f9e1895a0d78bdacd2a9bedf941de26b48786657
junryuf2 <junryuf2@illinois.edu>
2022-04-14 22:35:40 +0000

    changes made on parsing routes

--- parsing.cpp
+++ parsing.cpp
@@ -22,33 +22,55 @@ Airport Parsing::createAirport(vector<string> line) {
   string id_ = line[4].substr(1, 3);
   double lat_ = std::stod(line[6]);
   double long_ = std::stod(line[7]);
-  Airport airport = Airport(id_, lat_, long_);
+  int index = airport_map.size();
+  Airport airport = Airport(id_, lat_, long_, index);
   return airport;
 }
 
 bool Parsing::isInUS(vector<string> line) {
   string country = line[3].substr(1, line[3].size() - 2);
-  if (country == "United States") {return true;}
+  if (country == "United States") {
+    return true;
+  }
   return false;
 }
 
 void Parsing::extractAirports(string fileName) {
   FILE * data = fopen(fileName.c_str(), "r");
   string line = "";
-  while(!feof(data)) {
-    line.push_back(getc(data));
-    if (getc(data) == '\n') {
-      vector<string> converted = parseLine(line);
-      if (isInUS(converted)) {
-        Airport airport = createAirport(converted);
-        if (airport_map.find(airport.getID()) == airport_map.end()) {
-          airport_map[airport.getID()] = airport;
-        }
+
+  ifstream myAirports(fileName);
+  if(myAirports.is_open()) {
+      while(getline(myAirports, line)) {
+        std::cout << line << std::endl;
+          vector<string> parsed = parseLine(line);
+          std::cout << "country is ";
+          std::cout << parsed[3] << std::endl;
+          if(isInUS(parsed)) {
+            Airport airport = createAirport(parsed);
+            if(airport_map.find(airport.getID()) == airport_map.end()) {
+              airport_map[airport.getID()] = airport;
+            }
+          }
       }
-      line = "";
-    }
+      myAirports.close();
   }
-  fclose(data);
+  // while(!feof(data)) {
+  //   line.push_back(getc(data));
+  //   std::cout << line << std::endl;
+  //   if (getc(data) == '\n') {
+  //     vector<string> converted = parseLine(line);
+  //     // std::cout << converted[3] << std::endl;
+  //     if (isInUS(converted)) {
+  //       Airport airport = createAirport(converted);
+  //       if (airport_map.find(airport.getID()) == airport_map.end()) {
+  //         airport_map[airport.getID()] = airport;
+  //       }
+  //     }
+  //     line = "";
+  //   }
+  // }
+  // fclose(data);
 }
 /**
  * @brief all the United States routes are extracted
@@ -67,8 +89,16 @@ vector<Routes> Parsing::extractRoutes(string fileName) {
           //should sort or decide whether the airports both are in the US
           if((airport_map.find(parsed[2]) != airport_map.end()) && (airport_map.find(parsed[4]) != airport_map.end())) {
               //that means the airport exists in the map, only then I can add 
-              routeList.push_back(createRoutes(parsed));
+              Routes route = createRoutes(parsed);
+              string routeID = route.getDeparture() + route.getDestination();
+              if(route_map.find(routeID) == route_map.end()) {
+
+                //420 means that it exists
+                route_map[routeID] = 420;
+                routeList.push_back(route);
+              }
           }
+          
 
       }
       myroutes.close();
